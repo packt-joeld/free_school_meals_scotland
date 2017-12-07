b49e007d095f9a878c7f75a23cb8535ec428eaa2
packt-joeld <joeld@packtpub.com>
2017-12-07 17:53:51 +0530

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,45 +1,38 @@
-# Data stored on individual pages not accessible from one single
-#page typical URL is https://paulbradshaw.github.io/scraping-for-everyone/scottishschools/iSchoolid_5237521.html
-#Need to cycle through a list of those codes
-#If you want to understand this scraper - start at the bottom 
-#where it says 'base_url' (line 40 or so)
-
+########################
+# We use a ScraperWiki library called pdftoxml to scrape PDFs
+# This is an example of scraping a simple PDF
+########################
 import scraperwiki
-import lxml.html
-
-#Create a function called 'scrape_table' which is called in the 
-#function 'scrape_page' below
-#The 'scrape_page' function also passed the contents of the page 
-#to this function as 'root
-def scrape_table(root):
-#Use cssselect to find the contents of a particular HTML tag, and 
-#put it in a new object 'rows'
-
-#there's more than one table, so we need to specify the 
-#class="destinations", represented by the full stop
-    rows = root.cssselect("table.destinations tr")
-    for row in rows:
-        #Create a new empty record
-        record = {}
-        #Assign the contents of <td to a new object called
-        #'table_cells' 
-        table_cells = row.cssselect("td")
-        #If there's anything
-        if table_cells:
-            #Put the contents of the first <td into a record in
-            #the column 'FSM'
-            record['FSM'] = table_cells[0].text
-            #this takes the ID number, which has been named item
-            #in the for loop below
-            record['ID'] = item
-            print record, '------------'
-            #Save in the SQLite database, with the ID code to be
-            #used as the unique reference
-            scraperwiki.sqlite.save(["ID"], record)
-#this creates a new function and (re)names whatever parameter is
-#passed to it - i.e. 'next_link' below - as 'url'
-def scrape_page(url):
-    #now 'url' is scraped with the scraperwiki library imported
-    #above, and the contents put into a new object, 'html'
-    html = scraperwiki.scrape(url)
-    print html
+import urllib2
+import lxml.etree
+url = "http://www.madingley.org/uploaded/Hansard_08.07.2010.pdf"
+pdfdata = urllib2.urlopen(url).read()
+print "The pdf file has %d bytes" % len(pdfdata)
+xmldata = scraperwiki.pdftoxml(pdfdata)
+print "After converting to xml it has %d bytes" % len(xmldata)
+print "The first 2000 characters are: ", xmldata[:2000]
+root = lxml.etree.fromstring(xmldata)
+pages = list(root)
+print "The pages are numbered:", [ page.attrib.get("number")  for\
+ page in pages ]
+#this function has to work recursively because we might have #"<bPart1 <ipart 2</i</b"
+def gettext_with_bi_tags(el): 
+    res = [ ]
+    if el.text:
+        res.append(el.text)
+    for lel in el:
+        res.append("<%s" % lel.tag)
+        res.append(gettext_with_bi_tags(lel))
+        res.append("</%s" % lel.tag)
+        if el.tail:
+            res.append(el.tail)
+    return "".join(res)
+# print the first hundred text elements from the first page
+page0 = pages[0]
+for el in list(page)[:100]:
+    if el.tag == "text":
+        print el.attrib, gettext_with_bi_tags(el)
+# If you have many PDF documents to extract data from, the trick
+#is to find what's similar in the way that the information is #presented in them in terms of the top left bottom right
+#pixel locations.  It's real work, but you can use the position #visualizer here:
+#    http://scraperwikiviews.com/run/pdf-to-html-preview-1/
